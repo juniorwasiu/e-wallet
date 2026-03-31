28a1e9a286def91e277d143435bdb7a9f27fef26
jaeybean <jaeybean12@jaey.mykampus.org>
2026-03-31 08:43:57 +0100

    Remove BuiltWithRocket telemetry scripts to isolate backend to Supabase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,8 +25,6 @@ export default function RootLayout({
                 <AuthProvider>
                     {children}
                 </AuthProvider>
-                <script type="module" async src="https://static.rocket.new/rocket-web.js?_cfg=https%3A%2F%2Fewallet9039back.builtwithrocket.new&_be=https%3A%2F%2Fappanalytics.rocket.new&_v=0.1.17" />
-                <script type="module" defer src="https://static.rocket.new/rocket-shot.js?v=0.0.2" />
             </body>
         </html>
     );
